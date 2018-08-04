e611a0a57c509bcec186aa984b97e79e041903d6
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-08-04 17:00:34 +0700

    - 05-service

---
+++ source demo/05-service/SampleRouter/src/app/services/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: Http) { }
+
+  post(url: string, data: any){
+    return this.http.post(url,data).toPromise();
+  }
+}
