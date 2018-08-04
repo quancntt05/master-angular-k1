7aa0d1196e1400a60aa0dcc63d4a1363173e8062
DESKTOP-OCN7DI8\tuanh <tuanhoang18061997@gmail.com>
2018-08-04 17:04:23 +0700

    Services

---
+++ students/Tuan_mf/04-router - Copy/sample-services/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HttpModule} from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RoleComponent } from './main/role/role.component';
+import { UserComponent } from './main/user/user.component';
+import { TestComponent } from './main/test/test.component';
+import { UtilityService } from './services/utility.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'main', component: MainComponent, children:
+      [
+        { path: 'role', component: RoleComponent },
+        { path: 'user', component: UserComponent },
+        { path: 'test', component: TestComponent },
+      ]
+  },
+  { path: 'login', component: LoginComponent }
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    RoleComponent,
+    UserComponent,
+    TestComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpModule
+  ],
+  providers: [UtilityService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
