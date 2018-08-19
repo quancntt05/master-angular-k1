66182edc8da6d0a66ea32b0c13d5e9b6da7412c5
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-08-19 14:58:05 +0700

    - Update demo 09

---
+++ source demo/09-training/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
+import { UtilityService } from './services/utility.service';
+import { ApiService } from './services/api.service';
+import { RoleDetailComponent } from './role/role-detail.component';
+import { NotifyService } from './services/notify.service';
+import { LoadingService } from './services/loading.service';
+import { UserDetailComponent } from './user/user-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  {
+    path: 'main', component: MainComponent, children:
+      [
+        { path: 'role', component: RoleComponent },
+        { path: 'role-detail/:id', component: RoleDetailComponent },
+        { path: 'user', component: UserComponent },
+        { path: 'user-detail/:id', component: UserDetailComponent }
+      ]
+  },
+  { path: 'login', component: LoginComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    RoleComponent,
+    RoleDetailComponent,
+    UserComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [UtilityService, NotifyService, LoadingService, ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ source demo/09-training/src/app/role/role.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { UtilityService } from '../services/utility.service';
+import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router';
+import { NotifyService } from '../services/notify.service';
+import { LoadingService } from '../services/loading.service';
+
+@Component({
+  selector: 'app-role',
+  templateUrl: './role.component.html',
+  styleUrls: ['./role.component.css']
+})
+export class RoleComponent implements OnInit {
+  items: any[] = [];
+  constructor(private router: Router,
+    private loadingService: LoadingService,
+    private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.loadingService.start();
+    this.apiService.post('api/role/all', null)
+      .then((res: any[]) => {
+        this.loadingService.stop();
+        this.items = res;
+      });
+  }
+
+  detail(item?: any) {
+    if (item) this.router.navigate(['main/role-detail', item.Id]);
+    else this.router.navigate(['main/role-detail', '']);
+  }
+
+  delete(item: any) {
+    this.apiService.delete(`api/role/delete?id=${item.Id}`).then(() => this.load());
+  }
+}
